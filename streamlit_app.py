8137e9dbaf721a6c6863bb97dcf93718eb6e9fbb
drubhattacharya <dru@nextgenmph.com>
2025-04-07 12:44:16 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,9 +44,8 @@ baseline_monthly_cost = (baseline_vri_minutes * baseline_vri_rate) + (baseline_p
 
 
 # Calculate projected losses or savings
-baseline_annual_cost = baseline_monthly_cost * 12
-projected_monthly_difference = baseline_monthly_cost - total_monthly_cost
-projected_annual_difference = baseline_annual_cost - total_annual_cost
+projected_monthly_difference = (baseline_vri_minutes * baseline_vri_rate + baseline_phone_minutes * baseline_phone_rate) - (vri_minutes * vri_rate + phone_minutes * phone_rate)
+projected_annual_difference = projected_monthly_difference * 12
 projected_monthly_difference = baseline_monthly_cost - total_monthly_cost
 projected_annual_difference = baseline_annual_cost - total_annual_cost
 

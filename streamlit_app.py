52f5b0c9c47f929b72ce2d9dd868a1b866584893
drubhattacharya <dru@nextgenmph.com>
2025-04-07 12:31:02 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,8 @@ total_monthly_cost = vri_cost + phone_cost
 total_annual_cost = total_monthly_cost * 12
 
 # Cost projections
+baseline_vri_rate = 0.85
+baseline_phone_rate = 0.80
 # Baseline cost is dynamically calculated using user-defined base minutes (reflecting evolving organizational needs)
 baseline_vri_minutes = base_minutes * (vri_percent / 100)
 baseline_phone_minutes = base_minutes * (phone_percent / 100)
